9d20b917b1fb6890f0d38ca871ecb3a8e03cfede
Jash0013 <143797467+JD1304@users.noreply.github.com>
2025-01-28 20:02:23 +0530

    Update data-type.java

--- Difficulty: Basic/Data Type/data-type.java
+++ Difficulty: Basic/Data Type/data-type.java
@@ -1,30 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.math.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        while (T-- > 0) {
-            sc.nextLine();
-            String s = sc.next();
-            Solution obj = new Solution();
-            int ans = obj.dataTypeSize(s);
-            String Character = "Character";
-            if (Character.equals(s) && ans == 2) {
-                ans = 1;
-            }
-            System.out.println(ans);
-        
-System.out.println("~");
-}
-    }
-}
 
 // } Driver Code Ends
 
