2a870a2b9fdfcae02eac2745929ec6a46a016529
Jash0013 <143797467+JD1304@users.noreply.github.com>
2025-02-11 19:47:26 +0530

    Create Table.java

---
+++ Difficulty: Basic/While Loop/Table.java
@@ -0,0 +1,13 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        int n = 10;
+        while(n>0)
+        {
+            System.out.println(n*2);
+            n--;
+        }
+    }
+}
