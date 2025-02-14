e8dd2ed02a3b728be55f62e2b06cdda892df7b2c
Jash0013 <143797467+JD1304@users.noreply.github.com>
2025-02-15 00:06:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse an Array/reverse-an-array.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        while (tc-- > 0) {
+            String str[] = br.readLine().split(" ");
+            int arr[] = new int[str.length];
+
+            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
+            Solution obj = new Solution();
+
+            obj.reverseArray(arr);
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public static void reverseArray(int arr[]) {
+        
+       
+
+        // Reverse the array
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            // Swap elements
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+        // Print the reversed array
+        
+    }
+    
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4}; // Input array
+        reverseArray(arr); // Corrected function call
+    }
+}
