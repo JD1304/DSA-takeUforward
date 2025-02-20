0386067ab8071ecd5d256a22934a02541b05823f
JashTech0013 <143797467+JD1304@users.noreply.github.com>
2025-02-20 20:07:43 +0530

    Create Solution.java

---
+++ Arrays/Majority Element/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        
+        int max = nums[0];
+        for(int i = 1; i < n; i++)
+        {
+            if(nums[i]==max)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+                if(count==0)
+                {
+                    max = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        
+        return max;
+        
+    }
+}
